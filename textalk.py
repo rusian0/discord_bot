d62783c414266e189389403a2f9534b4735e2620
in <in@verocity.jp>
2020-01-26 01:58:47 +0900

    語録内にある言葉を検知した場合に復唱させる処理追加

--- textalk.py
+++ textalk.py
@@ -1,21 +1,26 @@
 import discord
 import json
 
+
+# 設定ファイル用のconfig.jsonを読み込み辞書型にする
+json_file = open('config.json', 'r')
+config = json.load(json_file)
+
 client = discord.Client()
 @client.event
 async def on_ready():
     print('起動完了')
 
-# ユーザーがテキストチャンネルでチャットを送信したときのイベント　現在未使用
+# ユーザーがテキストチャンネルでチャットを送信したときのイベント
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
-        
-    print(message)
-    if message.content == '!vcset':
-        print(type(message.content))
-        await message.channel.send('test')
+
+    # ユーザーのチャット内容が語録に入っているものだったらBOTが復唱する
+    if message.content in config['analects']
+        await message.channel.send(message.content)
+    
 
 # ユーザーがボイスチャンネルに入った or 出たときのイベント
 @client.event
@@ -33,9 +38,6 @@ async def on_voice_state_update(member, before, after):
         # 特定のロールを削除する（特定ロールが既に付与されているかどうかに関わらず削除を実行する)
         await member.remove_roles(role)
 
-# 設定ファイル用のconfig.jsonを読み込み辞書型にする
-json_file = open('config.json', 'r')
-config = json.load(json_file)
 
 # トークンを使用したBOTの起動
 client.run(config['bot_token'])
