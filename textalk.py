06ec2ab377ca134d5ff22ea1f5656617f0d980be
in <in@verocity.jp>
2020-01-26 01:43:40 +0900

    コメント追加

--- textalk.py
+++ textalk.py
@@ -6,6 +6,7 @@ client = discord.Client()
 async def on_ready():
     print('起動完了')
 
+# ユーザーがテキストチャンネルでチャットを送信したときのイベント　現在未使用
 @client.event
 async def on_message(message):
     if message.author.bot:
@@ -16,18 +17,25 @@ async def on_message(message):
         print(type(message.content))
         await message.channel.send('test')
 
+# ユーザーがボイスチャンネルに入った or 出たときのイベント
 @client.event
 async def on_voice_state_update(member, before, after):
+    # 特定のロールの取得
     role = discord.utils.get(member.guild.roles, name='Talker')
+
+    # ユーザーが入ったボイスチャンネルの名前がパコるフィールドだった場合
     if str(after.channel) == 'パコるフィールド☆':
+        # 特定のロールをユーザーに付与する
         await member.add_roles(role)
-        print('ロール付与')
-
+        
+    # 指定するボイスチャンネル以外に入った or 又はボイスチャンネルから抜けた場合
     else:
+        # 特定のロールを削除する（特定ロールが既に付与されているかどうかに関わらず削除を実行する)
         await member.remove_roles(role)
-        print('ロール削除')
 
+# 設定ファイル用のconfig.jsonを読み込み辞書型にする
 json_file = open('config.json', 'r')
 config = json.load(json_file)
 
+# トークンを使用したBOTの起動
 client.run(config['bot_token'])
